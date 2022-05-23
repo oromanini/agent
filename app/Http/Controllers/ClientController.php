765f18827898727239c3f1c1b5a57cbd26528606
Alluz Energia <alluzenergia@gmail.com>
2022-05-23 11:36:05 -0300

    fix agent controller

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ClientRequest;
+use App\Models\Client;
+use App\Models\State;
 use App\Models\User;
 use App\Repositories\ClientRepository;
 use App\Services\ClientService;
+use Exception;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class ClientController extends Controller
@@ -25,4 +30,46 @@ class ClientController extends Controller
 
         return view('clients.index', compact('clients', 'agents'));
     }
+
+    public function create()
+    {
+        $states = State::all();
+
+        return view('clients.form', compact('states'));
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function store(ClientRequest $request): RedirectResponse
+    {
+        $request->validated();
+
+        $message = $this->clientService->store($request->all());
+
+        session()->flash('message', $message);
+
+        return redirect()->route('client.index');
+    }
+
+    public function edit($id)
+    {
+        $client = Client::find($id);
+        $states = State::all();
+        $address = $client->addresses->first();
+        $consumerUnit = $address->consumerUnit;
+
+        return view('clients.form', compact($this->setEditParams()));
+    }
+
+    private function setEditParams(): array
+    {
+        return [
+            'client',
+            'states',
+            'address',
+            'consumerUnit'
+        ];
+    }
 }
+
