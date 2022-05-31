c44fd422a642df9ec1e4c473e23cffff878cec2d
Alluz Energia <alluzenergia@gmail.com>
2022-05-31 14:43:29 -0300

    finish mobile

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,25 +8,10 @@
                 <div class="title-bottom-line"></div>
             </div>
             <div class="body">
-                <div class="columns">
-                    <div class="column is-4">
-                        <iframe  src="https://www.youtube.com/embed/QHA3yqrzc1E"
-                                title="YouTube video player" frameborder="0"
-                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                                allowfullscreen></iframe>
-                    </div>
-                    <div class="column is-4">
-                        <iframe src="https://www.youtube.com/embed/hbfBBpMcvOk"
-                                title="YouTube video player" frameborder="0"
-                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                                allowfullscreen></iframe>
-                    </div>
-                    <div class="column is-4"></div>
-                </div>
                 <div class="columns mt50">
-                    <h3 class="title">Por onde você quer começar?</h3>
+                    <img src="/img/logo/alluz-icon.png" width="40" alt=""><h3 class="title">Por onde você quer começar?</h3>
                 </div>
-                <div class="columns mt50 mb20" id="home-buttons">
+                <div class="columns mt50 mb50" id="home-buttons">
                     <div class="column is-flex is-justify-content-center">
                         <a href="{{ route('client.index') }}" class="p-yellow-button">
                             <ion-icon name="people-outline"></ion-icon>  Meus clientes
@@ -42,6 +27,22 @@
                             <ion-icon name="document-outline"></ion-icon>  Nova proposta
                         </a>
                     </div>
+                </div>
+
+                <div class="columns mt50">
+                    <div class="column is-4">
+                        <iframe  src="https://www.youtube.com/embed/QHA3yqrzc1E"
+                                title="YouTube video player" frameborder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                allowfullscreen></iframe>
+                    </div>
+                    <div class="column is-4">
+                        <iframe src="https://www.youtube.com/embed/hbfBBpMcvOk"
+                                title="YouTube video player" frameborder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                allowfullscreen></iframe>
+                    </div>
+                    <div class="column is-4"></div>
                 </div>
             </div>
         </div>
