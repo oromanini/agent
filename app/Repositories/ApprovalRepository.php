38ba1a6daa979de5f5f4ad7f66ef4a9d63860753
Alluz Energia <alluzenergia@gmail.com>
2022-07-09 23:04:43 -0300

    create approval repository with filters

---
+++ app/Repositories/ApprovalRepository.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Client;
+use App\Models\Proposal;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Facades\Auth;
+
+class ApprovalRepository implements Filter
+{
+    public function filter($data): LengthAwarePaginator
+    {
+        return Proposal::query()
+            ->where(function ($query) use($data) {
+                filterName($data, $query);
+                filterAgent($data, $query);
+                filterDocument($data, $query);
+            })
+            ->whereNotNull('send_date')
+            ->orderBy('id', 'desc')
+            ->paginate(20);
+    }
+}
