45a7e7e6bd833418596ba0000ddcc719a90e1d14
Alluz Energia <alluzenergia@gmail.com>
2022-07-10 00:05:10 -0300

    make technical tab on approval

---
+++ resources/views/approval/tabs/technical.blade.php
@@ -0,0 +1,20 @@
+<div class="columns is-flex is-flex-wrap-wrap" style="margin-top: 50px">
+    @include('pre_inspection.images')
+</div>
+
+@include('pre_inspection.images_script')
+
+<style>
+    .pre-inspection-video {
+        background-color: #a00000;
+        border-radius: 50px;
+        color: #fff;
+        padding: 5px 10px;
+    }
+
+    .oranged {
+        color: #ff6200;
+        font-weight: 800;
+    }
+
+</style>
