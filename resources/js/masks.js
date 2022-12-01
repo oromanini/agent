4d516699a426e2c7b77bccdef99e336ce2146e2a
Alluz Energia <alluzenergia@gmail.com>
2022-12-01 20:02:06 -0300

    fix delete client

--- resources/js/masks.js
+++ resources/js/masks.js
@@ -12,12 +12,18 @@ $(function () {
 
     $('#type').change(function () {
         if ($('#type').val() === 'company') {
-            $('#document').mask('00.000.000/0000-00', {reverse: true});
+
+            $('#cpf').attr("id", "cnpj")
+
+            $('#cnpj').mask('00.000.000/0000-00', {reverse: true});
             $('#nameLabel').text('Razão Social*')
             $('#documentLabel').text('CNPJ*')
             $('#aliasLabel').text('Nome Fantasia*')
         } else {
-            $('#document').mask('000.000.000-00', {reverse: true});
+
+            $('#cnpj').attr("id", "cpf")
+
+            $('#cpf').mask('000.000.000-00', {reverse: true});
             $('#nameLabel').text('Nome*')
             $('#documentLabel').text('CPF*')
             $('#aliasLabel').text('Apelido*')
