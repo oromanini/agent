75d316fd20fc6956eca3605c4cf7ec87f777ebc3
Alluz Energia <alluzenergia@gmail.com>
2023-02-14 01:00:28 -0300

    Create status mechanism

--- app/Repositories/HomologationRepository.php
+++ app/Repositories/HomologationRepository.php
@@ -12,7 +12,7 @@ class HomologationRepository
     : LengthAwarePaginator
     {
         return Homologation::query()->with(['proposal' => function ($query) {
-            $query->where('deleted_at', '=', null);
+            $query->onlyTrashed();
         }])
             ->where(function ($query) use($data) {
                 filterName($data, $query);
