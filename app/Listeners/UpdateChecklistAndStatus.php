df943b68071712dcaeabd105343ab13dd46e4af1
Oscar Romanini <alluzenergia@gmail.com>
2023-02-22 21:00:58 -0300

    navigate to technical

--- app/Listeners/UpdateChecklistAndStatus.php
+++ app/Listeners/UpdateChecklistAndStatus.php
@@ -3,25 +3,33 @@
 namespace App\Listeners;
 
 use App\Events\AfterSaleProcessDepartmentChanged;
+use Illuminate\Database\Eloquent\Model;
 
 class UpdateChecklistAndStatus extends AfterSaleProcessBase
 {
 
     public function handle(AfterSaleProcessDepartmentChanged $event): void
     {
-        $model = $event->model;
 
-//        $checklist = json_decode($model->checklist, true);
-        $helper = new ('App\\Helpers\\' . class_basename($model) . 'Helper');
+
+        $helper = new ('App\\Helpers\\' . class_basename($event->model) . 'Helper');
 
         foreach ($helper::setListenedFields() as $field) {
-            isset($model->$field)
+
+            $updatedModel = self::setModel($event->model);
+
+            isset($updatedModel->$field)
             && $this->setStatusAndMarkItem(
                 item: $field,
-                model: $model,
-                checklist: json_decode($model->checklist, true),
+                model: $updatedModel,
+                checklist: json_decode($updatedModel->checklist, true),
                 helper: $helper
             );
         }
+    }
+
+    private static function setModel(Model $model)
+    {
+        return ('App\\Models\\' . class_basename($model))::find($model->id);
     }
 }
