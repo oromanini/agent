f4c3b51e9de0986906b4ed3d468692cce70f3824
oromanini <oscar.romanini.jr@gmail.com>
2023-12-21 12:49:39 -0300

    change availability

--- app/Packages/EdeltecApiPackage/EdeltecApiService.php
+++ app/Packages/EdeltecApiPackage/EdeltecApiService.php
@@ -149,12 +149,9 @@ class EdeltecApiService
 
         if (!is_null($kit)) {
             $kit->cost = $item['precoDoIntegrador'];
-            $kit->availability = $days_to_availability;
-//            $days_to_availability <= self::DAYS_FOR_INACTIVE &&
+            $kit->availability = $item['dataPrevistaParaDisponibilidade'];
             $kit->update();
         } else {
-//            $days_to_availability <= self::DAYS_FOR_INACTIVE
-//            &&
             Kit::create($this->setKitParams($item));
         }
     }
