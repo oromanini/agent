e9f5b28afe6e36a926810457b0a1786641a6aef9
oromanini <oscar.romanini.jr@gmail.com>
2024-07-22 19:14:28 -0300

    show all projects to internals

--- app/Repositories/ProposalRepository.php
+++ app/Repositories/ProposalRepository.php
@@ -8,19 +8,22 @@ use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 
 class ProposalRepository implements Filter
 {
-    public function filter($data)
+    public function filter($data): LengthAwarePaginator
     {
         return Proposal::query()->where('deleted_at', '=', null)
-            ->where(function ($query) use($data) {
+            ->where(function($query) use($data) {
                 filterName($data, $query);
                 filterAgent($data, $query);
                 filterDocument($data, $query);
                 filterInitialDate($data, $query);
                 filterFinalDate($data, $query);
-                filterPermission($data, $query);
+            })
+            ->where(function($query) use($data) {
+                if (auth()->user()->permission == 'agent') {
+                    $query->where('agent_id', \auth()->user()->id);
+                }
             })
             ->orderBy('proposals.id', 'desc')
-
             ->paginate(10);
     }
 }
