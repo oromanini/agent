0456c7930c66c186ee14f04f37a416f9dfb02e75
oromanini <alluzenergia@gmail.com>
2025-05-06 19:09:45 -0300

    add-longi-kits

--- app/Packages/EdeltecApiPackage/Enums/PanelBrand.php
+++ app/Packages/EdeltecApiPackage/Enums/PanelBrand.php
@@ -6,22 +6,24 @@ use Illuminate\Support\Facades\Log;
 
 enum PanelBrand: string
 {
-    case HANERSUN = 'HANERSUN TOPCON BIFACIAL';
+    case LONGI = 'LONGI HPBC';
+//    case HANERSUN = 'HANERSUN TOPCON BIFACIAL';
 //    case HONOR = 'HONOR';
-    case OSDA = 'OSDA TOPCON BIFACIAL';
+//    case OSDA = 'OSDA TOPCON BIFACIAL';
 //    case SINE = 'SINE';
-    case RESUN = 'RESUN TOPCON';
+//    case RESUN = 'RESUN TOPCON';
 //    case RONMA = 'RONMA';
 
     public static function matchCases(string $panel): PanelBrand
     {
         return match (strtoupper($panel)) {
+            'LONGI' => self::LONGI,
 //            'HONOR' => self::HONOR,
-            'OSDA TOPCON BIFACIAL' => self::OSDA,
+//            'OSDA TOPCON BIFACIAL' => self::OSDA,
 //            'SINE' => self::SINE,
-            'RESUN TOPCON' => self::RESUN,
+//            'RESUN TOPCON' => self::RESUN,
 //            'RONMA' => self::RONMA,
-            'HANERSUN TOPCON BIFACIAL' => self::HANERSUN,
+//            'HANERSUN TOPCON BIFACIAL' => self::HANERSUN,
             default => throw new \InvalidArgumentException("Panel brand '$panel' is not recognized."),
         };
     }
