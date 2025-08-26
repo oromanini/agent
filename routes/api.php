8838922f557bcc56c99b08d5d7235f5ee06d09a9
oromanini <alluzenergia@gmail.com>
2025-08-26 20:08:08 -0300

    remove auth

--- routes/api.php
+++ routes/api.php
@@ -11,17 +11,19 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/soollar/update-products', [SoollarController::class, 'index']);
-    Route::post('/edeltec/update-products', [EdeltecController::class, 'index']);
 
-    Route::prefix('brands')->group(function () {
-        Route::post('/{type}', [BrandController::class, 'store'])->name('api.brands.store');
-        Route::put('/{type}/{id}', [BrandController::class, 'update'])->name('api.brands.update');
-        Route::delete('/{type}/{id}', [BrandController::class, 'destroy'])->name('api.brands.destroy');
-        Route::patch('/{type}/{id}/toggle', [BrandController::class, 'toggleActive'])->name('api.brands.toggle');
-    });
-
-    Route::put('/active-kits/{activeKit}/toggle', [ActiveKitController::class, 'toggleActive']);
 });
+
+Route::post('/soollar/update-products', [SoollarController::class, 'index']);
+Route::post('/edeltec/update-products', [EdeltecController::class, 'index']);
+
+Route::prefix('brands')->group(function () {
+    Route::post('/{type}', [BrandController::class, 'store'])->name('api.brands.store');
+    Route::put('/{type}/{id}', [BrandController::class, 'update'])->name('api.brands.update');
+    Route::delete('/{type}/{id}', [BrandController::class, 'destroy'])->name('api.brands.destroy');
+    Route::patch('/{type}/{id}/toggle', [BrandController::class, 'toggleActive'])->name('api.brands.toggle');
+});
+
+Route::put('/active-kits/{activeKit}/toggle', [ActiveKitController::class, 'toggleActive']);
 
 Route::post('/authorize', [AuthController::class, 'login']);
